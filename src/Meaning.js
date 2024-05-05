f016aff9557aef4e09cb83b21c0c2eaeaf85940b
anasam31 <navratil.ana@gmail.com>
2024-05-05 17:43:38 +0200

    added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,10 +2,8 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-
   return (
-    <div className="Meanings">
+    <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
