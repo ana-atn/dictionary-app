77bfad778ab13ef5951243efb768393bc75f1a68
anasam31 <navratil.ana@gmail.com>
2024-05-05 14:43:27 +0200

    added alt to image

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ export default function App() {
   return (
     <div className="App">
       <header>
-        <img src={book} width="40px" />
+        <img src={book} width="40px" alt="Dictionary Image" />
       </header>
       <h1>{""} Dictionary</h1>
+
+      <footer>
+        <a
+          href="https://github.com/ana-atn/dictionary-app"
+          target="_blank"
+          rel="no-referrer"
+        >
+          open-source code
+        </a>{" "}
+        by ana atanackovic
+      </footer>
     </div>
   );
 }
